51c99cab070f47cfcfa718631715c7b1fd61e1e0
Luca Puddu <info@lucapuddu.com>
2024-02-09 17:11:27 +0800

    Remove test code

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -13,8 +13,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        Log::debug('hello');
-
         return $request->expectsJson() ? null : route('login');
     }
 }
